0fdf1d2acdde4ef9b24cce10a37473c9ceb28751
Alvaro Condori <97042212+ACondori95@users.noreply.github.com>
2025-05-27 21:56:19 -0300

    Update axiosInstance.js

--- usuario/src/utils/axiosInstance.js
+++ usuario/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://sami-baby-shower-backend.onrender.com",
   headers: {"Content-Type": "application/json"},
 });
 
